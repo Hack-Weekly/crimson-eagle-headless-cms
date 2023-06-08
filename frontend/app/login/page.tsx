4373fcac095742ae55ad89dc555aa0983cc23c20
Muhammad Jaafar <mohamadjafer14235@gmail.com>
2023-06-09 02:07:32 +0300

    add form component

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type pageProps = {};
+
+const page: React.FC<pageProps> = () => {
+  return (
+    <div
+      className="
+    flex items-center justify-center h-screen
+    mx-auto w-full	max-w-2xl  
+    "
+    ></div>
+  );
+};
+export default page;
