d5f6590f48faab267375f64de52b612f64e35da8
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-06-09 16:05:17 -0400

    added missing configs

---
+++ CMS.API/DataAccessLayer/Interfaces/IcmsProjectRepository.cs
@@ -0,0 +1,9 @@
+﻿using CMS.API.DataAccessLayer.Models;
+using System.Diagnostics.Metrics;
+
+namespace CMS.API.DataAccessLayer.Interfaces
+{
+    public interface IcmsProjectRepository : IGenericRepository<cmsProject>
+    {
+    }
+}
