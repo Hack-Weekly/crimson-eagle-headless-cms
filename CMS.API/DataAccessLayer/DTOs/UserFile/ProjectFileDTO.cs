8e7facadbf45774cf655d2409e5832f7b1c2bf32
FrolickingMustelid <kszolgyen@gmail.com>
2023-06-18 20:04:33 +0200

    Add media ID to response

--- CMS.API/DataAccessLayer/DTOs/UserFile/ProjectFileDTO.cs
+++ CMS.API/DataAccessLayer/DTOs/UserFile/ProjectFileDTO.cs
@@ -4,10 +4,11 @@ namespace CMS.API.DataAccessLayer.DTOs.UserFile
 {
     public class ProjectFileDTO : UpdateProjectFileDTO
     {
+        public int Id { get; set; }
         public DateTime UploadedAt { get; set; }
 
-        public UploadResult Image { get; set; }
         public required string UploadedById { get; set; }
         public required string cmsProjectId { get; set; }
+        public UploadResult Image { get; set; }
     }
 }
