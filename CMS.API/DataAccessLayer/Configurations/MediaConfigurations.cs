5556f0158aa66891f55b9f9c5e511dc36d1cdcb7
FrolickingMustelid <kszolgyen@gmail.com>
2023-06-19 19:34:13 +0200

    Media seeds

---
+++ CMS.API/DataAccessLayer/Configurations/MediaConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using CMS.API.DataAccessLayer.Models;
+
+namespace CMS.API.DataAccessLayer.Configurations
+{
+    public class MediaConfigurations : IEntityTypeConfiguration<UploadResult>
+    {
+        public void Configure(EntityTypeBuilder<UploadResult> builder)
+        {
+            builder.HasData(
+                    new UploadResult
+                    {
+                        PublicId = "zub4iesjxuq8z47601aw",
+                        Width = 500,
+                        Height = 500,
+                        Format = "jpg",
+                        ResourceType = "image",
+                        Url = new Uri("http://res.cloudinary.com/dgxfgifvw/image/upload/v1687115656/zub4iesjxuq8z47601aw.jpg"),
+                        SecureUrl = new Uri("https://res.cloudinary.com/dgxfgifvw/image/upload/v1687115656/zub4iesjxuq8z47601aw.jpg")
+                    }
+                );
+        }
+    }
+}
