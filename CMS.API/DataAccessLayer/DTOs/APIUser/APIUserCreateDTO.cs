cd039523d963b0a48909148877b09ea711c51116
FrolickingMustelid <kszolgyen@gmail.com>
2023-06-19 19:31:14 +0200

    User create bug fix

--- CMS.API/DataAccessLayer/DTOs/APIUser/APIUserCreateDTO.cs
+++ CMS.API/DataAccessLayer/DTOs/APIUser/APIUserCreateDTO.cs
@@ -2,23 +2,15 @@
 
 namespace CMS.API.DataAccessLayer.DTOs.APIUser
 {
-    public class APIUserCreateDTO
+    public class APIUserCreateDTO : APIUserLoginDTO
     {
-        [Required(ErrorMessage = "Please provide your e-mail address.")]
-        [EmailAddress]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Please provide a valid e-mail address.")]
-        public required string Email { get; set; }
-        [Required(ErrorMessage = "Please provide your first name.")]
+        [Required(ErrorMessage = "Please provide a first name.")]
         [MaxLength(256, ErrorMessage = "The first name value cannot exceed 255 characters.")]
         public required string FName { get; set; }
-        [Required(ErrorMessage = "Please provide your last name.")]
+        [Required(ErrorMessage = "Please provide a last name.")]
         [MaxLength(256, ErrorMessage = "The first name value cannot exceed 255 characters.")]
         public required string LName { get; set; }
         public string? OrganizationName { get; set; }
-        [Required(ErrorMessage = "Please provide your password.")]
-        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
-        [DataType(DataType.Password)]
-        public required string Password { get; set; }
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         [Compare("Password", ErrorMessage = "Password and Confirmation Password must match.")]
         [DataType(DataType.Password)]
--- CMS.API/DataAccessLayer/DTOs/APIUser/APIUserLoginDTO.cs
+++ CMS.API/DataAccessLayer/DTOs/APIUser/APIUserLoginDTO.cs
@@ -4,11 +4,11 @@ namespace CMS.API.DataAccessLayer.DTOs.APIUser
 {
     public class APIUserLoginDTO
     {
-        [Required(ErrorMessage = "Please provide your e-mail address.")]
+        [Required(ErrorMessage = "Please provide an e-mail address.")]
         [EmailAddress]
         [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Please provide a valid e-mail address.")]
         public required string Email { get; set; }
-        [Required(ErrorMessage = "Please provide your password.")]
+        [Required(ErrorMessage = "Please provide a password.")]
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         [DataType(DataType.Password)]
         public required string Password { get; set; }
