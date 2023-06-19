6f2b208c2a35672635728f07a21f5c64b106fc9a
FrolickingMustelid <kszolgyen@gmail.com>
2023-06-19 19:33:51 +0200

    Media DTO missing Image bug fix

--- CMS.API/DataAccessLayer/DTOs/UserFile/UpdateProjectFileDTO.cs
+++ CMS.API/DataAccessLayer/DTOs/UserFile/UpdateProjectFileDTO.cs
@@ -1,4 +1,6 @@
 using CMS.API.DataAccessLayer.Enumerables;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System.ComponentModel.DataAnnotations;
 
 namespace CMS.API.DataAccessLayer.DTOs.UserFile
@@ -8,6 +10,8 @@ namespace CMS.API.DataAccessLayer.DTOs.UserFile
         [Required(ErrorMessage = "Please provide a title.")]
         [MaxLength(256, ErrorMessage = "The title value cannot exceed 255 characters.")]
         public required string Title { get; set; }
+        [JsonProperty("category")]
+        [JsonConverter(typeof(StringEnumConverter))]
         public FileCategory? Category { get; set; }
         [MaxLength(256, ErrorMessage = "The description value cannot exceed 255 characters.")]
         public string? Description { get; set; }
